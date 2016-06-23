c22ad08989904e8ef21d30475d6e5326ff7fd583
Luis Alberto Osinaga <luis.osinaga@gmail.com>
2016-06-23 11:15:21 -0400

    BackupAll and Cash Flow Instalador Manual

---
+++ Backups-All/Automate/Batch-File/12AM-copia.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+REM ---------------------------------
+
+copy C:\Backups-All\Backup\12AM\*.bak \\SERVIDOR\NOVUS\CashFlow_Backups 
+copy C:\Backups-All\Backup\12AM\*.bak \\ADMINISTRACION\contabilidad\CashFlow_Backups 
+
+REM ---------------------------------
---
+++ Backups-All/Automate/Batch-File/12PM-copia.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+REM ---------------------------------
+
+copy C:\Backups-All\Backup\12PM\*.bak \\SERVIDOR\NOVUS\CashFlow_Backups 
+copy C:\Backups-All\Backup\12PM\*.bak \\ADMINISTRACION\contabilidad\CashFlow_Backups 
+
+REM ---------------------------------
